7df730338ddab8f3a5e0becad40ebdaeb4d863af
anzaniy <78955667+Nadaa14@users.noreply.github.com>
2022-10-08 17:15:37 +0700

    login dan login css

---
+++ app/Views/user/login.php
@@ -0,0 +1,40 @@
+
+<?php
+// session_start();
+// if(isset($_SESSION['login'])){
+//   header('Location: index.php');
+//   exit;
+// }
+?>
+<?= $this -> extend('template/header')?>
+<?= $this-> section('content')?>
+    <section>
+       <div class="imgBx">
+           <img src="assets/Arsha/assets/img/bus.jpg" alt="image login">
+       </div>
+       <div class="contentBx">
+           <div class="formBx">
+               <h2>Login</h2>
+               <form action="control_login.php" METHOD="post">
+                   <div class="inputBx">
+                       <span>Username</span>
+                       <label>
+                           <input type="text" name="username" required autocomplete="off">
+                       </label>
+                   </div>
+                   <div class="inputBx">
+                       <span>Password</span>
+                       <label>
+                           <input type="password" name="password" required autocomplete="off">
+                       </label>
+                   </div>
+                   <div class="inputBx">
+                       <input type="submit" value="Sign In" name="login">
+                   </div>
+               </form>
+           </div>
+       </div>
+    </section>
+  </body>
+</html>
+<?= $this -> endSection() ?>
