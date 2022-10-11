674c82f7c3e9db828f6b3b21f1e59afaab1abd74
Irfan Saputra <75194907+irfanshow@users.noreply.github.com>
2022-10-11 18:52:42 +0700

    rapihin

--- app/Controllers/user/FunctionUser.php
+++ app/Controllers/user/FunctionUser.php
@@ -83,36 +83,7 @@ class FunctionUser extends BaseController
             $session ->setFlashdata('msg','User Tidak Ditemukan');
             return redirect() -> to('/view_login');
         }
-        // $um = new UserModels();
-        // $dataa['akun_user'] = $um->findAll();
-        // $conn = db_connect();
-        // if (isset($_POST["login"])){
-        //     $email = $_POST["email"];
-        //     $password = $_POST["password"];
-        //     // $result = mysqli_query($conn,"SELECT * FROM akun_user WHERE email = '$email'");
-        //     $result = $conn->query("SELECT * FROM akun_user WHERE email = '$email'");
-        //     if($result===1){
-        //       $row = mysqli_fetch_assoc($result);
-        //       // if($password=== $row["password"]){
-        //       //   header("Location: index.php");
-        //       //   exit;
-        //       // }
-        //        if(password_verify($password,$row["password"])){
-        //           $_SESSION["login"] = $row["id_akun"];
-        //         //   header("Location: index.php");
-        //         //   exit();
-        //             echo 'Berhasil Login';
-        
-        //         }
-        //         else{
-        //             echo "<script>
-        //                 alert('Password Salah');
-        //                 </script>";
-        //         }
-                
-        //     }
-        
-        // }
+
     }
     public function Register_User(){
         // protected $allowedFields = ['username', 'password', 'id_pelanggan', 'email','nama_pelanggan','nomor_hp_pelanggan'];
