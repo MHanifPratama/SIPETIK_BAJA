ad8dd9fb637f360cb8fc4cd388ec852edd9a93ca
msdzilii <83410300+msdzilii@users.noreply.github.com>
2022-10-24 23:35:40 +0700

    buat tampilan login register admin2

--- app/Views/admin/loginRegister/registerAdmin.php
+++ app/Views/admin/loginRegister/registerAdmin.php
@@ -8,39 +8,43 @@
 					<span class="login100-form-title p-b-26">
 						Welcome
 					</span>
-					<span class="login100-form-title p-b-48">
-						<i class="zmdi zmdi-font"></i>
-					</span>
 
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is: a@b.c">
 						<input class="input100" type="text" name="email">
 						<span class="focus-input100" data-placeholder="Email"></span>
 					</div>
 
+					<div class="wrap-input100 validate-input" data-validate = "Valid username is: a@b.c">
+						<input class="input100" type="text" name="username">
+						<span class="focus-input100" data-placeholder="Username"></span>
+					</div>
+
 					<div class="wrap-input100 validate-input" data-validate="Enter password">
-						<span class="btn-show-pass">
-							<i class="zmdi zmdi-eye"></i>
-						</span>
 						<input class="input100" type="password" name="pass">
 						<span class="focus-input100" data-placeholder="Password"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="conf password">
+						<input class="input100" type="password" name="pass">
+						<span class="focus-input100" data-placeholder="Confirmasi Password"></span>
 					</div>
 
 					<div class="container-login100-form-btn">
 						<div class="wrap-login100-form-btn">
 							<div class="login100-form-bgbtn"></div>
 							<button class="login100-form-btn">
-								Login
+								Register
 							</button>
 						</div>
 					</div>
 
 					<div class="text-center p-t-115">
 						<span class="txt1">
-							Don’t have an account?
+							have account?
 						</span>
 
-						<a class="txt2" href="#">
-							Sign Up
+						<a class="txt2" href="/loginAdmin">
+							Sign In
 						</a>
 					</div>
 				</form>
