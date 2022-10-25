cb3d5987abe83ab58e84d0ae44595fb63ddb32d1
MHanifPratama <mhanifp27@gmail.com>
2022-10-25 15:41:55 +0700

    Dashboard Sederhana

--- app/Views/template/headerAdmin.php
+++ app/Views/template/headerAdmin.php
@@ -15,7 +15,6 @@
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
   <!-- Vendor CSS Files -->
   <link href="/assetsAdmin/NiceAdmin/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="/assetsAdmin/NiceAdmin/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
@@ -176,7 +175,7 @@
   <script src="/assetsAdmin/NiceAdmin/assets/vendor/simple-datatables/simple-datatables.js"></script>
   <script src="/assetsAdmin/NiceAdmin/assets/vendor/tinymce/tinymce.min.js"></script>
   <script src="/assetsAdmin/NiceAdmin/assets/vendor/php-email-form/validate.js"></script>
-
+  
   <!-- Template Main JS File -->
   <script src="/assetsAdmin/NiceAdmin/assets/js/main.js"></script>
   <?= $this->renderSection('contentAdmin') ?>
