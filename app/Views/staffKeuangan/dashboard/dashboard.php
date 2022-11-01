90a3fa55daba90fbca4a63067b5067684d5a8b13
msdzilii <83410300+msdzilii@users.noreply.github.com>
2022-11-01 13:02:06 +0700

    staff keuangaan

--- app/Views/staffKeuangan/dashboard/dashboard.php
+++ app/Views/staffKeuangan/dashboard/dashboard.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?= $this -> extend('template/temDashboard')?>
+<?= $this-> section('contentStaff')?>
+
+<main id="main" class="main">
     <h1>Ini adalah halaman dashboard</h1>
-</body>
+    
+</main>
 </html>
+<?= $this -> endSection() ?>
