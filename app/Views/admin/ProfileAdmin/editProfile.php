3a3fcbf2a152615ae6b2554c06f27c0b04b831f8
msdzilii <83410300+msdzilii@users.noreply.github.com>
2022-11-01 12:28:36 +0700

    Update editProfile.php

--- app/Views/admin/ProfileAdmin/editProfile.php
+++ app/Views/admin/ProfileAdmin/editProfile.php
@@ -1,16 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<form action ="\updateProfileAdmin\<?= $data['admin']['id_akun_admin'] ?>" method ="post">
-  <label for="fname">Username:</label><br>
-  <input type="text" id="username" name="username" value="<?=$data['admin']['username']?>"><br>
-  <button type="submit" class="btn btn-outline-primary">Submit</button>
-</form>
-</body>
-</html>
+<?= $this -> extend('template/headerAdmin')?>
+<?= $this-> section('contentAdmin')?>
+
+<main id="main" class="main">
+  <div class="pagetitle">
+      <h1>My Profile</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="/dashboard">Home</a></li>
+          <li class="breadcrumb-item active">My Profile</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+  <form action ="\updateProfileAdmin\<?= $data['admin']['id_akun_admin'] ?>" method ="post">
+    <div class="form-row">
+      <div class="form-group col-md-6 mb-3">
+        <label for="fname">Username:</label><br>
+        <input type="text" class="form-control" id="username" name="username" value="<?=$data['admin']['username']?>"><br>
+      </div>
+    </div>
+    <button type="submit" class="btn btn-outline-primary">Submit</button>
+  </form>
+</main>
+
+
+<?= $this -> endSection() ?>
